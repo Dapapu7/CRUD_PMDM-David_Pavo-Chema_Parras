042286d1ddd2c7401f9d4625a3a685826ff7c832
Dapapu7 <dapapu7@gmail.com>
2021-05-10 19:55:03 +0200

    App.java nuevo con Update, Insert, Read y Listar

--- JDBCDavidChema/src/main/java/com/ejemplo/JDBCDavidChema/App.java
+++ JDBCDavidChema/src/main/java/com/ejemplo/JDBCDavidChema/App.java
@@ -1,6 +1,7 @@
 package com.ejemplo.JDBCDavidChema;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import com.ejemplo.JDBCDavidChema.Entities.Order;
 import com.ejemplo.JDBCDavidChema.Models.OrdersModel;
@@ -15,14 +16,40 @@ public class App {
 		
 			System.out.println("Conexion a la BBDD con éxito");
 			
+//			Esto es el read de pedidos {
 			pedido = misPedidos.read(30);  //Leemos el pedido con id = 30
-			
 			if(pedido != null)  //Si el pedido es distinto de null
 				System.out.println(pedido.toString());  //Convertimos el pedido a String
-
+//			}
+			
+			Integer id = misPedidos.insert(pedido);
+			System.out.println("Acabo de insertar el pedido: "+ id);
+			
+			//Ahora leo el cliente recien insertado y le cambio la compañia
+			pedido = misPedidos.read(id);
+			pedido.setPayment_type("hola");
+			
+			System.out.println();
+			System.out.println();
+			System.out.println("Ahora vamos a leer el cliente");
+			
+			ArrayList<Order> pedidos = misPedidos.lista("id>40", 10, 2);
+			listaPedidos(pedidos);
+			
 		} catch(SQLException e) {
 			System.err.println("No se ha podido conectar a la BBDD " + e.getMessage());
 			System.exit(1);
+		}
+	}
+	
+	static void listaPedidos(ArrayList<Order> pedidos) {
+    	if(pedidos == null) {
+    		System.out.println("No hay elementos en la lista");
+    		return;
+    	}
+ 
+		for(Order pedido : pedidos) {
+			System.out.println(pedido.toString());
 		}
 	}
 
--- JDBCDavidChema/src/main/java/com/ejemplo/JDBCDavidChema/Models/OrdersModel.java
+++ JDBCDavidChema/src/main/java/com/ejemplo/JDBCDavidChema/Models/OrdersModel.java
@@ -1,9 +1,11 @@
 package com.ejemplo.JDBCDavidChema.Models;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import javax.sql.DataSource;
 
@@ -48,6 +50,7 @@ public class OrdersModel {
 		try {
 			statement = conexion.createStatement();  //Creamos la sentencia
 			ResultSet myResultSet = statement.executeQuery(sql);  //Ejecutamos la sentencia y la guardamos en forma de "tabla"
+			//Se recorre el resultSet
 			while (myResultSet.next()) {
 				pedido = new Order(
 					myResultSet.getInt("id"),
@@ -80,9 +83,66 @@ public class OrdersModel {
 		return pedido;
 	}
 
-	public Integer insert(Order pedido) {
+	public Integer insert(Order pedido) throws SQLException {
 		Integer id = null;
-
+		PreparedStatement ps = null;
+		
+		String sql = "INSERT INTO orders ("
+				+ "`employee_id`, "
+				+ "`customer_id`, "
+				+ "`order_date`, "
+				+ "`shipped_date`, "
+				+ "`shipper_id`, "
+				+ "`ship_name`, "
+				+ "`ship_address`, "
+				+ "`ship_city`, "
+				+ "`ship_state_province`, "
+				+ "`ship_zip_postal_code`, "
+				+ "`ship_country_region`, "
+				+ "`shipping_fee`, "
+				+ "`taxes`, "
+				+ "`payment_type`, "
+				+ "`paid_date`, "
+				+ "`notes`, "
+				+ "`tax_rate`, "
+				+ "`tax_status_id`, "
+				+ "`status_id`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+		
+		try {
+			ps = conexion.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			
+			ps.setInt(1, pedido.getEmployee_id());
+			ps.setInt(2, pedido.getCustomer_id());
+			ps.setDate(3, pedido.getOrder_date());
+			ps.setDate(4, pedido.getShipped_date());
+			ps.setInt(5, pedido.getShipper_id());
+			ps.setString(6, pedido.getShip_name());
+			ps.setDate(7, pedido.getShipped_date());
+			ps.setString(8, pedido.getShip_city());
+			ps.setString(9, pedido.getShip_state_province());
+			ps.setString(10, pedido.getShip_zip_postal_code());
+			ps.setString(11, pedido.getShip_country_region());
+			ps.setBigDecimal(12, pedido.getShipping_fee());
+			ps.setBigDecimal(13, pedido.getTaxes());
+			ps.setString(14, pedido.getPayment_type());
+			ps.setDate(15, pedido.getPaid_date());
+			ps.setString(16, pedido.getNotes());
+			ps.setDouble(17, pedido.getTax_rate());
+			ps.setBoolean(18, pedido.getTax_status_id());
+			ps.setBoolean(19, pedido.getStatus_id());
+			
+			if (ps.executeUpdate() > 0) {
+				ResultSet rs = ps.getGeneratedKeys();
+				if (rs.next()) 
+				    id = rs.getInt(1);
+			}
+			
+		} catch (SQLException e) {
+			
+			System.err.println("Fallo al insertar pedido " + e.getMessage());
+			throw e;
+			
+		}
 		return id;
 	}
 
@@ -96,5 +156,76 @@ public class OrdersModel {
 		Boolean resultado = false;
 
 		return resultado;
+	}
+	
+	public ArrayList<Order> lista(String filtro, Integer limite, Integer offset){
+		
+		ArrayList<Order> pedido = new ArrayList<Order>();
+		Statement statement = null;
+		
+		String sql = "SELECT `id`, "
+				+ "`employee_id`, "
+				+ "`customer_id`, "
+				+ "`order_date`, "
+				+ "`shipped_date`, "
+				+ "`shipper_id`, "
+				+ "`ship_name`, "
+				+ "`ship_address`, "
+				+ "`ship_city`, "
+				+ "`ship_state_province`, "
+				+ "`ship_zip_postal_code`, "
+				+ "`ship_country_region`, "
+				+ "`shipping_fee`, "
+				+ "`taxes`, "
+				+ "`payment_type`, "
+				+ "`paid_date`, "
+				+ "`notes`, "
+				+ "`tax_rate`, "
+				+ "`tax_status_id`, "
+				+ "`status_id`"
+				+ " FROM orders ";
+		
+		try {
+			if(filtro != null)
+				sql += "WHERE " + filtro;
+			if(limite != null)
+				sql += " LIMIT " + limite;
+			if(offset != null)
+				sql += " OFFSET " + offset;
+			
+			statement = conexion.createStatement();
+			ResultSet rs = statement.executeQuery(sql);
+			
+			while(rs.next()) {
+				pedido.add(new Order(
+					rs.getInt("id"),
+					rs.getInt("employee_id"),
+					rs.getInt("customer_id"),
+					rs.getDate("order_date"),
+					rs.getDate("shipped_date"),
+					rs.getInt("shipper_id"),
+					rs.getString("ship_name"),
+					rs.getString("ship_address"),
+					rs.getString("ship_city"),
+					rs.getString("ship_state_province"),
+					rs.getString("ship_zip_postal_code"),
+					rs.getString("ship_country_region"),
+					rs.getBigDecimal("shipping_fee"),
+					rs.getBigDecimal("taxes"),
+					rs.getString("payment_type"),
+					rs.getDate("paid_date"),
+					rs.getString("notes"),
+					rs.getDouble("tax_rate"),
+					rs.getBoolean("tax_status_id"),
+					rs.getBoolean("status_id")
+				));
+				
+			}
+			
+		}catch(SQLException e) {
+			System.err.println("Error al listar pedido " + e.getMessage());
+			return null;
+		}
+		return pedido;
 	}
 }
